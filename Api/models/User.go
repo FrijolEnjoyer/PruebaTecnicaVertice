e32b3a0f3c7d729bff8cc9087a1f4c24bbf512d6
Santiago Gonzalez Noreña <santiago.gonzalez@s4learning.com>
2025-07-08 23:57:15 -0500

    Changed gorm.Model for correct tests operations

--- Api/models/User.go
+++ Api/models/User.go
@@ -1,12 +1,19 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model   `json:"-" swaggerignore:"true"`
-	Username     string `gorm:"type:varchar(255);uniqueIndex" json:"username"`
-	Password     string `validate:"required,password" json:"password" bson:"password"`
-	Email        string `gorm:"type:varchar(255);uniqueIndex" json:"email" bson:"email"`
-	RefreshToken string `json:"refresh_token"`
-	Token        string `json:"token"`
+	ID           uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	Username     string         `gorm:"type:varchar(255);uniqueIndex" json:"username"`
+	Password     string         `validate:"required,password" json:"password,omitempty" bson:"password"`
+	Email        string         `gorm:"type:varchar(255);uniqueIndex" json:"email" bson:"email"`
+	RefreshToken string         `json:"refresh_token,omitempty"`
+	Token        string         `json:"token,omitempty"`
 }
