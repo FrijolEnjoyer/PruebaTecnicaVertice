3607104a48b0c43a696bdde3ffce1b0cf8ec622f
Santiago Gonzalez Noreña <santiago.gonzalez@s4learning.com>
2025-07-08 23:54:53 -0500

    Script updated for go run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo "🔍 Ejecutando tests..."
+echo "🔍 Ejecutando tests con cobertura..."
 
-# Ejecuta los tests con salida detallada (-v)
-go test ./... -v
+go test -v -coverprofile=coverage.out ./...
 
-# Guarda el código de salida
+
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -eq 0 ]; then
   echo "✅ Todos los tests pasaron correctamente."
+  echo "📊 Reporte de cobertura:"
+  go tool cover -func=coverage.out
 else
   echo "❌ Algunos tests fallaron."
 fi
+
 
 exit $EXIT_CODE
